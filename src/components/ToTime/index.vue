dd8ad86256b6dd28c211923448e25bb62dfec16c
Minal C <mdchhatbar@gmail.com>
2023-03-20 09:38:58 -0400

    Initial commit

---
+++ src/components/ToTime/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="to-time">
+    <MilitaryTime :time="time" />
+    <CivilianTime :time="time" />
+    <QuarterTime :time="time" />
+    <!-- <ContextualTime :time="time" /> -->
+  </div>
+</template>
+
+<script>
+  import MilitaryTime from './MilitaryTime';
+  import CivilianTime from './CivilianTime';
+  import QuarterTime from './QuarterTime';
+
+  export default {
+    name: 'ToTime',
+    props: { time: { type: Date, required: true } },
+    components: { MilitaryTime, CivilianTime, QuarterTime }
+  }
+</script>
+
+<style scoped>
+  .to-time {
+    font-size: 3.6vw;
+  }
+</style>
