50744cfc7696f4cb21e0e68a55d4fcb2b682d21c
Minal C <mdchhatbar@gmail.com>
2023-03-20 09:38:59 -0400

    Initialize main

---
+++ src/components/ToTime/MilitaryTime/MilitaryHour.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="military-hour">{{ hourPhrase }}</div>
+</template>
+
+<script>
+  const frenchHours = ['zéro', 'une', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix', 'onze', 'douze', 'treize', 'quatorze', 'quinze', 'seize', 'dix-sept', 'dix-huit', 'dix-neuf', 'vingt', 'vingt-et-une', 'vingt-deux', 'vingt-trois'];
+
+  export default {
+    name: 'MilitaryHour',
+    props: { time: { type: Date, required: true } },
+    computed: {
+      hourPhrase () {
+        let datetime = new Date(this.time),
+          hour = datetime.getHours(),
+          suffix = 'heure' + ([0, 1].includes(hour) ? '' : 's');
+
+        return `${frenchHours[hour]} ${suffix}`;
+      }
+    }
+  }
+</script>
+
+<style scoped></style>
